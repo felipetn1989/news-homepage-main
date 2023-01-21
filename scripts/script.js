2f3144a0c27f2c0d1135240dcf9c2db7a1937f8d
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-01-21 09:22:47 -0300

    readme

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,19 @@
-let menu = document.querySelector(".menu")
+let menu = document.querySelector(".menu");
 
-menuIcon.addEventListener("click",toggleMenu)
+let overlay = document.querySelector(".overlay");
 
-function toggleMenu () {
-    if (window.getComputedStyle(menu).display == 'none') {
-        menu.style.display = 'flex'
-        menuIcon.src = "assets/images/icon-menu-close.svg";
-        menuIcon.style.height = '1.675rem'
-    } else {
-        menu.style.display = 'none'
-        menuIcon.src = "assets/images/icon-menu.svg";
-        menuIcon.style.height = 'unset'
-    }
+menuIcon.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (window.getComputedStyle(menu).display == "none") {
+    menu.style.display = "flex";
+    menuIcon.src = "assets/images/icon-menu-close.svg";
+    menuIcon.style.height = "1.675rem";
+    overlay.style.display = "block";
+  } else {
+    menu.style.display = "none";
+    menuIcon.src = "assets/images/icon-menu.svg";
+    menuIcon.style.height = "unset";
+    overlay.style.display = "none";
+  }
 }
