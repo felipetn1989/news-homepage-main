9b74b313b1f1537c8ef1c03cebc62ec5d1bfaefb
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-01-21 08:53:49 -0300

    started mobile menu

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,11 @@ menuIcon.addEventListener("click",toggleMenu)
 function toggleMenu () {
     if (window.getComputedStyle(menu).display == 'none') {
         menu.style.display = 'flex'
+        menuIcon.src = "assets/images/icon-menu-close.svg";
+        menuIcon.style.height = '1.675rem'
     } else {
         menu.style.display = 'none'
+        menuIcon.src = "assets/images/icon-menu.svg";
+        menuIcon.style.height = 'unset'
     }
 }
